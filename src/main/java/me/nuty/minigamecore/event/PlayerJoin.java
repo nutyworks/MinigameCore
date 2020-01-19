3a65ef5a504ac2d30192153c8b04466324201929
nutyworks <nutyworks@gmail.com>
2020-01-19 12:02:16 +0900

    add lobby setting when player join

--- src/main/java/me/nuty/minigamecore/event/PlayerJoin.java
+++ src/main/java/me/nuty/minigamecore/event/PlayerJoin.java
@@ -1,6 +1,8 @@
 package me.nuty.minigamecore.event;
 
 import me.nuty.minigamecore.MinigameCore;
+import me.nuty.minigamecore.util.Constant;
+import org.bukkit.GameMode;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
@@ -8,6 +10,14 @@ import org.bukkit.event.player.PlayerJoinEvent;
 public class PlayerJoin implements Listener {
     @EventHandler
     public void onJoin(PlayerJoinEvent event) {
+
+        event.getPlayer().setInvulnerable(true);
+        event.getPlayer().setFlying(false);
+        event.getPlayer().setAllowFlight(false);
+        event.getPlayer().setHealth(20);
+        event.getPlayer().setSaturation(20);
+        event.getPlayer().teleport(Constant.LOBBY);
+        event.getPlayer().setGameMode(GameMode.ADVENTURE);
 
         MinigameCore.getInstance().getLobbyScoreboard().display(event.getPlayer());
     }
